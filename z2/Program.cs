0c4c1b87a781a6a4d6d460f07e04fe0f70d26551
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2021-10-27 22:09:31 +0300

    1

---
+++ z2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\dz2\\";
+            
+
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("Каталоги:");
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
